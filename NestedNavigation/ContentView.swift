954745eaa661ac079c27dd0f0f84f47e56162ebf
seem <mwlorgat@gmail.com>
2023-01-28 18:44:11 +0200

    working nested NavigationStack

--- NestedNavigation/ContentView.swift
+++ NestedNavigation/ContentView.swift
@@ -1,26 +1,39 @@
-//
-//  ContentView.swift
-//  NestedNavigation
-//
-//  Created by Wasim Lorgat on 2023/01/28.
-//
-
 import SwiftUI
 
+struct Folder: Hashable, Identifiable {
+    var id = UUID()
+    let name: String
+    let contents: [Folder]
+}
+
 struct ContentView: View {
+    let folder = Folder(name: "Root", contents: [
+        Folder(name: "First", contents: [
+            Folder(name: "Second", contents: [])
+        ])
+    ])
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            // WARNING: Update NavigationRequestObserver tried to update multiple times per frame.
+            ZStack { // Why is this needed to make warning disappear?
+                FolderView(folder: folder)
+                    .navigationDestination(for: Folder.self) { folder2 in
+                        FolderView(folder: folder2)
+                    }
+            }
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct FolderView: View {
+    let folder: Folder
+
+    var body: some View {
+        List(folder.contents) { folder in
+            NavigationLink(value: folder) {
+                Text(folder.name)
+            }
+        }
     }
 }
