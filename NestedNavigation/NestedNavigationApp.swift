4806c10f2f3bac7caf3a06cdce53ed69f1679a4e
seem <mwlorgat@gmail.com>
2023-01-28 18:26:20 +0200

    Initial Commit

---
+++ NestedNavigation/NestedNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestedNavigationApp.swift
+//  NestedNavigation
+//
+//  Created by Wasim Lorgat on 2023/01/28.
+//
+
+import SwiftUI
+
+@main
+struct NestedNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
